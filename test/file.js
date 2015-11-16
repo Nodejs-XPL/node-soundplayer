262c1ee74fac8c535e5f8abc45f2c9bcadc75da8
oeuillot <olivier.oeuillot@gmail.com>
2015-11-16 15:37:07 +0100

    Fix code

--- test/file.js
+++ test/file.js
@@ -1,14 +1,14 @@
 var Play = require('../lib/play');
 
-var play=new Play();
+var play = new Play();
 
-var p = play.sound("../wavs/drums/kick.wav", function(error) {
+play.on("progress", function onProgress(percent) {
+  console.log("progress", percent);
+});
+
+play.sound("../wavs/drums/kick.wav", function(error) {
   if (error) {
     console.error(error);
     return;
   }
-});
-
-p.on("progress", function onProgress(percent) {
-  console.log("progress", percent);
 });
