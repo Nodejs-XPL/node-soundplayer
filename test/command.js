aee6bbf56db111b9d0d99d52d1acc243bab9a75b
oeuillot <olivier.oeuillot@gmail.com>
2015-11-18 16:52:10 +0100

    Fix code

--- test/command.js
+++ test/command.js
@@ -1,20 +1,24 @@
 var Path = require('path');
 var Player = require('../lib/player');
+var commander = require('commander');
 
 var player = new Player();
 
-var path = process.argv[1];
+commander.command('*').description("Start playing").action(function(args) {
 
-console.log("Play ", path);
-var sound = player.playSound(path, function(error) {
-  if (error) {
-    console.error(error);
-    return;
-  }
+  console.log("Play ", args);
+  var path = args[0];
+
+  var sound = player.playSound(path, function(error) {
+    if (error) {
+      console.error(error);
+      return;
+    }
+  });
+
+  sound.on("progress", function onProgress(percent) {
+    console.log("progress", percent);
+  });
+
+  console.log("UUID of sound=" + sound.uuid);
 });
-
-sound.on("progress", function onProgress(percent) {
-  console.log("progress", percent);
-});
-
-console.log("UUID of sound=" + sound.uuid);
